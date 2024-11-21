cca82e59fb24e18aa0e957c7094a88e7d199fdef
Felics666 <felics6@gmail.com>
2024-11-22 01:38:12 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyproTermPaperExamQuestions/SkyproTermPaperExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproTermPaperExamQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproTermPaperExamQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproTermPaperExamQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyproTermPaperExamQuestions/SkyproTermPaperExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproTermPaperExamQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproTermPaperExamQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
